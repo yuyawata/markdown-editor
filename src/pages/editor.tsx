947455451dd0a6d4e18224b4a0764df152c4ca7a
Yutaka Yawata <yuyawata@gmail.com>
2021-05-12 07:36:19 +0900

    Add a preview  function

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useStateWithStorage } from "../hooks/use_state_with_storage";
+import * as ReactMarkdown from "react-markdown";
 
 const Header = styled.header`
   font-size: 1.5rem;
@@ -58,7 +59,9 @@ export const Editor: React.FC = () => {
           }}
           value={text}
         />
-        <Preview>プレビューエリア</Preview>
+        <Preview>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </Preview>
       </Wrapper>
     </>
   );
